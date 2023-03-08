95cb96fae3f9e0919d5a47a9774d0389026ee344
Ishwar6010 <guptaishwar6010@gmail.com>
2023-03-08 09:25:53 +0530

    Add your commit

---
+++ GrpcClient/GrpcClient/Program.cs
@@ -0,0 +1,34 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using Grpc.Net.Client;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GrpcClient
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:44307/");
+            var client = new GrpcClientCount.ClientCountProvider.ClientCountProviderClient(channel);
+
+            var token = new CancellationTokenSource(TimeSpan.FromSeconds(5));
+            using var population = client.GetClientCount(
+                new Empty(),
+                cancellationToken: token.Token);
+            try
+            {
+                await foreach (var item in population.ResponseStream.ReadAllAsync(token.Token))
+                { 
+                    Console.WriteLine(item.Count);
+                }
+            }
+            catch (RpcException exc)
+            {
+                Console.WriteLine(exc.Message);
+            }
+        }
+    }
+}
---
+++ Server/Server/clientCountProviderService.cs
@@ -0,0 +1,20 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using GrpcClientCount;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public class clientCountProviderService:ClientCountProvider.ClientCountProviderBase
+    {
+        public override async Task GetClientCount(Empty request, IServerStreamWriter<ClientCount> responseStream, ServerCallContext context)
+        {
+            var count = 0;
+            while(!context.CancellationToken.IsCancellationRequested)
+            {
+                await responseStream.WriteAsync(new ClientCount { Count = count });
+                count++;
+            }
+        }
+    }
+}
